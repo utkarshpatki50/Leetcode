75dd16dcf067e623802c3c0dbbe93e71bb03a1f8
Utkarsh Patki <utkarshpatki50@gmail.com>
2023-09-16 20:28:41 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,23 @@
-class Solution {
-public:
+class Solution
+{
+    public:
 
-  int rob(vector<int> &nums)
-    {
-        int n = nums.size();
-        int prev1 = nums[0];
-        int prev = 0;
-        int curri = 0;
-        for (int i = 1; i < n; i++)
+        int rob(vector<int> &nums)
         {
-            int pick = nums[i];
-            if (i > 1)
-                pick = pick + prev;
-            int notPick = 0 + prev1;
-            curri = max(pick, notPick);
-            prev = prev1;
-            prev1 = curri;
+            int n = nums.size();
+            int prev = nums[0];
+            int prev2 = 0;
+            int curri = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int pick = nums[i];
+                if (i > 1)
+                    pick = pick + prev2;
+                int notPick = 0 + prev;
+                curri = max(pick, notPick);
+                prev2 = prev;
+                prev = curri;
+            }
+            return prev;
         }
-        return prev1;
-    }
-
 };
