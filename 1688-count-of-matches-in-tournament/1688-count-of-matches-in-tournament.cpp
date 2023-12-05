491a0f5bb1cd82b77296a5fe548346348ae07834
Utkarsh Patki <utkarshpatki50@gmail.com>
2023-12-06 00:17:05 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (36.15%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -3,22 +3,20 @@ class Solution
     public:
         int numberOfMatches(int n)
         {
-            int ans = 0;
-            while (n > 1)
+            int matches = 0;
+            while (n != 1)
             {
                 if (n % 2 == 0)
                 {
+                    matches = matches + (n / 2);
                     n = n / 2;
-
-                    ans += n;
                 }
                 else
                 {
-
-                    n = ((n - 1) / 2) + 1;
-                    ans += n - 1;
+                    matches = matches + (n - 1) / 2;
+                    n = n / 2 + 1;
                 }
             }
-            return ans;
+            return matches;
         }
 };
